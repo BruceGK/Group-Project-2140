a14c3850619fb5b8b443134ec8a30e050e733693
Miguel Chan <vvchan@outlook.com>
2021-11-03 00:18:21 -0400

    feat: update paper data processor

--- cord_ir/search/data_loader.py
+++ cord_ir/search/data_loader.py
@@ -17,10 +17,12 @@ class DataLoader:
                 res.append(line)
         return res
 
+    # load metadata list
     def load_metadata(self) -> pd.DataFrame:
         res = pd.read_csv(path.join(self.subfolder, 'metadata.csv'), dtype=str)
         return res
 
+    # load relevance score from TREC-COVID for evaluation
     def load_relevance(self) -> list:
         res = []
         with open(path.join(self.subfolder, 'eval', 'qrels-covid_d5_j0.5-5.txt'), 'r') as f:
@@ -34,6 +36,7 @@ class DataLoader:
                 })
         return res
 
+    # load paper data from file
     def load_paper_data(self, row) -> dict:
         file_path = ''
         if isinstance(row, str):
@@ -46,14 +49,32 @@ class DataLoader:
             return None
         with open(path.join(self.subfolder, file_path), 'r') as f:
             data = json.load(f)
-            data['main_text'] = self.get_full_body_text(data)
-            return data
+            main_text = self.get_full_body_text(data)
+            data['metadata'] = {**data['metadata'], **row.to_dict()}
+            return self.prepare_paper_data(data, main_text)
 
-    def get_full_body_text(self, paperData) -> str:
-        if paperData is None:
+    # take out the text part from paper's body
+    def get_full_body_text(self, paper_data) -> str:
+        if paper_data is None:
             return None
-        body = paperData['body_text']
+        body = paper_data['body_text']
         return ' '.join(map(lambda e: e['text'], body))
+
+    # prepare data for indexing
+    def prepare_paper_data(self, paper_data, main_text):
+        return {
+            'docno': paper_data['metadata']['cord_uid'],
+            'sha1': paper_data['paper_id'],
+            'authors': ' '.join(
+                [' '.join(
+                    [author['first'], author['middle'], author['last']]
+                ) for author in paper_data['metadata']['authors']]),
+            'data': {
+                'title': paper_data['metadata']['title'],
+                'abstract': paper_data['metadata']['abstract'],
+                'main_text': main_text,
+            }
+        }
 
 
 def main():
