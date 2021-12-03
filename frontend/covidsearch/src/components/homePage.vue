ea193cc3f2d5f246335acf988a2a3d3f582ceb4e
Miguel Chen <vvchan@outlook.com>
2021-12-02 23:52:24 -0500

    fix: fix naive ui import

--- frontend/covidsearch/src/components/homePage.vue
+++ frontend/covidsearch/src/components/homePage.vue
@@ -1,22 +1,59 @@
 <template>
-    <n-input-group>
-      <n-input :style="{ width: '50%' }" />
-      <n-button type="primary" ghost>Search</n-button>
-    </n-input-group>
+  <n-form class="search-wrapper">
+    <n-form-item>
+      <n-input-group>
+        <n-input
+          v-model:value="queryStr"
+          class="search-input"
+        />
+        <n-button
+          attr-type="submit"
+          @click="onSearch"
+          class="search-btn"
+          type="primary"
+          ghost
+        >
+          Search
+        </n-button>
+      </n-input-group>
+    </n-form-item>
+  </n-form>
 </template>
 
 <script>
+import service from "../utils/network";
+import { ref } from "vue";
 export default {
-  name: 'homePage',
+  name: "homePage",
   props: {
-    msg: String
-  }
-
-  
-}
+    msg: String,
+  },
+  setup() {
+    const queryStr = ref("");
+    const onSearch = async () => {
+      console.log(queryStr.value);
+      const resp = await service({
+        method: "get",
+        url: "/search",
+        params: {
+          q: queryStr.value,
+        },
+      });
+      console.log(resp);
+    };
+    return {
+      queryStr,
+      onSearch,
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.search-wrapper {
+  width: 50%;
+  display: block;
+  margin: 0 auto;
+}
 </style>
--- frontend/covidsearch/src/main.js
+++ frontend/covidsearch/src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import naive from 'naive-ui'
 
-createApp(App).mount('#app')
 const app = createApp(App)
 app.use(naive)
+app.mount('#app')
