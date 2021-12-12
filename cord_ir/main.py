2678899a3fd0b6db3b449ef3bf5d8f0a97a5390f
Miguel Chen <vvchan@outlook.com>
2021-12-11 22:24:30 -0500

    feat: add boolean search

--- cord_ir/main.py
+++ cord_ir/main.py
@@ -4,6 +4,8 @@ from search.ml_rank import MlRanker
 from search.elastic_index_reader import IndexReader
 from search.data_loader import DataLoader
 from os import getenv
+from flask import jsonify
+
 
 app = Flask(__name__)
 DATA_DIR = getenv("CORD_DIR")
@@ -12,6 +14,8 @@ loader.load_metadata_mappings(loader.load_metadata())
 ranker = MlRanker("../data/models/ranker.joblib",
                   '../data/models/tfidf.joblib',
                   "../data/models/docMatrix.joblib")
+reader = IndexReader()
+app.logger.info('ES info: %s' % reader.info())
 
 
 @app.route("/")
@@ -19,17 +23,64 @@ def test():
     return "Test"
 
 
-@app.route("/search")
+@app.route("/boolean", methods=['post'])
+def boolean():
+    data = request.json
+    queries = data['queries']
+    esQueries = {
+        "bool": {
+            "should": [],
+        }
+
+    }
+    queryBuild = esQueries['bool']['should']
+    reserved = '+ - = && || > < ! ( ) { } [ ] ^ " ~ * ? : \ /'.split(' ')
+    for q in queries:
+        for char in reserved:
+            q['query'].replace(char, '\\' + char)
+        if 'type' not in q or q['type'].lower() == 'and':
+            queryBuild.append({
+                "bool": {
+                    "must": [
+                        {"term": {q['field']: q['query']}}
+                    ]
+                }
+            })
+        elif q['type'].lower() == 'or':
+            queryBuild.append({
+                "bool": {
+                    "should": [
+                        {"term": {q['field']: q['query']}},
+                    ]
+                }
+            })
+        elif q['type'] == 'not':
+            queryBuild.append({
+                "bool": {
+                    "must_not": [
+                        {"term": {q['field']: q['query']}},
+                    ]
+                }
+            })
+        else:
+            return 'unexpected query ' + jsonify(q), 400
+    reader = IndexReader()
+    start = data['start']
+    print(esQueries)
+    result = reader.boolean_search("cord_test", esQueries, start_from=start)
+    return result['hits']
+
+
+@ app.route("/search")
 def search():
     query = request.args.get("q")
-    start = request.args.get("start")
     reader = IndexReader()
     start = request.args.get("start")
     result = reader.search("cord_test", query, start_from=start)
     return result['hits']
 
 
-@app.route("/mlsearch")
+@ app.route("/mlsearch")
 def mlsearch():
     query = request.args.get("q")
     start = request.args.get("start")
@@ -47,7 +98,7 @@ def mlsearch():
     return result['hits']
 
 
-@app.route("/details/<paperId>")
+@ app.route("/details/<paperId>")
 def detail(paperId):
     data = loader.load_paper_data(paperId)
     return data
--- cord_ir/search/elastic_index_reader.py
+++ cord_ir/search/elastic_index_reader.py
@@ -19,9 +19,20 @@ class IndexReader:
         reserved = '+ - = && || > < ! ( ) { } [ ] ^ " ~ * ? : \ /'.split(' ')
         for char in reserved:
             query = query.replace(char, '\\' + char)
-        highlight_config = {"fields": {"main_text": {
-            "number_of_fragments": 1,
-            "fragment_size": 150}}} if highlight else None
+        highlight_config = {"fields": {
+            "main_text": {
+                "number_of_fragments": 1,
+                "fragment_size": 150
+            },
+            "abstract": {
+                "number_of_fragments": 1,
+                "fragment_size": 150
+            },
+            "title": {
+                "number_of_fragments": 1,
+                "fragment_size": 150
+            }
+        }} if highlight else None
         return self.es.search(
             index=index_name,
             _source=False,
@@ -30,6 +41,32 @@ class IndexReader:
             highlight=highlight_config,
             fields=fields,
             query={"query_string": {"query": query}}
+        )
+
+    def boolean_search(self, index_name, query, start_from=0, size=20, fields=["title"], highlight=True):
+        reserved = '+ - = && || > < ! ( ) { } [ ] ^ " ~ * ? : \ /'.split(' ')
+        highlight_config = {"fields": {
+            "main_text": {
+                "number_of_fragments": 1,
+                "fragment_size": 150
+            },
+            "abstract": {
+                "number_of_fragments": 1,
+                "fragment_size": 150
+            },
+            "title": {
+                "number_of_fragments": 1,
+                "fragment_size": 150
+            }
+        }} if highlight else None
+        return self.es.search(
+            index=index_name,
+            _source=False,
+            from_=start_from,
+            size=size,
+            highlight=highlight_config,
+            fields=fields,
+            query=query
         )
 
     def tokenize(self, sentence, index_name='cord_test'):
